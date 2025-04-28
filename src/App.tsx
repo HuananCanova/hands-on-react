7850665d25c016beca351bef636b44b81623aeaa
huanan <haugustocanova@gmail.com>
2025-04-28 08:53:15 -0300

    first commit - 28

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <blockquote cite="Benjamin Franklin">
+        Tell me and I forget, teach me and I may remember, involve me and I learn.
+      </blockquote>
+  )
+}
+
+export default App
