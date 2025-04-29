9d8adbb5b52890720342b28c9bce2b28b2263d55
huanan <haugustocanova@gmail.com>
2025-04-29 16:47:41 -0300

    new changes until hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import ProjectsPage from './project/ProjectsPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-      <blockquote cite="Benjamin Franklin">
-        Tell me and I forget, teach me and I may remember, involve me and I learn.
-      </blockquote>
+    <div className='container'>
+      <ProjectsPage />
+    </div>
   )
 }
 
