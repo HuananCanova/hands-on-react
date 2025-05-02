5812edfb91dc8861c9c10ed1be9509b131542527
huanan <haugustocanova@gmail.com>
2025-05-02 17:23:08 -0300

    started hooks and some button changes

--- src/project/ProjectList.tsx
+++ src/project/ProjectList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Project } from "./Project";
 import ProjectCard from "./ProjectCard";
 import ProjectForm from "./ProjectForm";
@@ -7,12 +8,26 @@ interface ProjectListProps{
 }
 
 function ProjectList ({ projects }: ProjectListProps){
+    const [projectBeingEdited, setProjectBeingEdited] = useState({});
+    
+    const handleEdit = ( project: Project) => {
+        setProjectBeingEdited(project);
+        console.log(project);   
+    };
+
+    const cancelEditing = () => {
+        setProjectBeingEdited({});
+    } ;
+
     return (
         <div className="row">
             {projects.map((project) => (
                 <div key={project.id} className="cols-sm">
-                    <ProjectCard project={project}/>
-                    <ProjectForm />
+                    {project === projectBeingEdited ? (
+                        <ProjectForm onCancel={cancelEditing}/>
+                    ): (
+                        <ProjectCard project={project} onEdit={handleEdit} />
+                    )}
                 </div>
             ))}
         </div>
